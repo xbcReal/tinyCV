c263c9622a4934ed4fa75216a6a7e0a9439abe4f
xbc <1191689945@qq.com>
2017-09-27 18:37:50 +0800

    add inv func

--- core/matrix.hpp
+++ core/matrix.hpp
@@ -265,7 +265,84 @@ namespace tinycv {
 			return 1.0 / v*(*this);
 		}
 
+		//matrix inv,until now,we only support square matrix to get its inv matrix and if the dimension is a little bigger ,the program would be pretty slow
+    //because until now we use adjoint matrix to get its inv matrix.And we will try to use other methods to get the inv matrix to support the none square
+    //and biiger matrix to get its inv matrix
+    Matrix inv(){
+        assert(_cols == _rows);
+        int n = _cols;
+        int i,j,k,m=2*n;
+        Matrix a(_cols,2 * _cols);
+        Matrix B(_cols,_cols);
+        double mik,temp;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==j)
+                    B[i][j]=1.0;
+                else
+                    B[i][j]=0.0;
+            }
+        }                                //初始化B=E
 
+        int iii= 0;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                a[i][j]=(*this)[i][j];  //复制A到a，避免改变A的值
+                //iii++;
+            }
+        }
+
+        for(i=0;i<n;i++)
+            for(j=n;j<m;j++)
+                a[i][j]=B[i][j-n];      //复制B到a，增广矩阵
+
+        for(k=1;k<=n-1;k++)
+        {
+            for(i=k+1;i<=n;i++)
+            {
+                mik=a[i-1][k-1]/a[k-1][k-1];
+                for(j=k+1;j<=m;j++)
+                {
+                    a[i-1][j-1]-=mik*a[k-1][j-1];
+                }
+            }
+        }                              //顺序高斯消去法化左下角为零
+
+        for(i=1;i<=n;i++)
+        {
+            temp=a[i-1][i-1];
+            for(j=1;j<=m;j++)
+            {
+                a[i-1][j-1]/=temp;
+            }
+        }                             //归一化
+
+        for(k=n-1;k>=1;k--)
+        {
+            for(i=k;i>=1;i--)
+            {
+                mik=a[i-1][k];
+                for(j=k+1;j<=m;j++)
+                {
+                    a[i-1][j-1]-=mik*a[k][j-1];
+                }
+            }
+        }                            //逆序高斯消去法化增广矩阵左边为单位矩阵
+
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                if(fabs(a[i][j+n]) < 0.0001){
+                    B[i][j]=0.0;
+                }
+                else{
+                    B[i][j] = a[i][j+n];
+                }
+            }
+        }
+        return B;
+    }
 	private:
 		void default_init(int rows,int cols)
 		{
